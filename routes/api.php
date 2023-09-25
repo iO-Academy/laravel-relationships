12c43a4f28d92ad7a489c750f363eb9c20d478c5
Ashley Coles <ashley@acoles.co.uk>
2023-09-25 08:57:55 +0100

    Cleaning up routes

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,7 @@
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('/products', [ProductController::class, 'getAll']);
 Route::post('/products', [ProductController::class, 'add']);
